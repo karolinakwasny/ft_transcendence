44aff01856fe5baee4bc18fd124d21a1d2391481
Karolina Kwasny <kkwasny@c2r3s6.42wolfsburg.de>
2024-08-13 21:05:42 +0200

    prepared gateway, connects to localhost:8000, I was able to log in to django

--- app/game_service/my_django_project/settings.py
+++ app/game_service/my_django_project/settings.py
@@ -91,8 +91,8 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = 'static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Directory where static files will be collected
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
