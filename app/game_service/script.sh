8a6e5113bd810a945519ad7428149d42e56e80f2
Karolina Kwasny <kkwasny@c2r4s3.42wolfsburg.de>
2024-08-14 15:41:22 +0200

    script for log in django with env

---
+++ app/game_service/script.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+python3 manage.py makemigrations
+python3 manage.py migrate --noinput
+
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ]; then
+    python manage.py createsuperuser \
+        --noinput \
+        --username "$DJANGO_SUPERUSER_USERNAME" \
+        --email "$DJANGO_SUPERUSER_EMAIL" \
+    || true
+fi
+
+python3 manage.py runserver 0.0.0.0:8000
