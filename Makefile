1cca6214c46aa7597064f09dce9429f0d90e7c45
Niimphu <nimphu@outlook.com>
2024-08-29 00:40:05 +0200

    1am mess

--- Makefile
+++ Makefile
@@ -1,16 +1,14 @@
 .PHONY: up down logs setup
 
-# Define a setup task that runs the setup_venv.sh scripts
-setup:
-	cd pageA && ./setup_venv.sh
-	cd pageB && ./setup_venv.sh
-
-# Define an up task that calls setup and then brings up Docker containers
-up: setup
+up:
 	docker-compose up --build
 
 down:
 	docker-compose down
+
+clean: down
+	docker-compose rm -f
+	docker system prune -a --volumes
 
 logs:
 	docker-compose logs -f
--- pageB/entrypoint.sh
+++ pageB/entrypoint.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
+
+# Check if virtual environment directory exists
+if [ ! -d "venv" ]; then
+    # Create a virtual environment
+    python3 -m venv venv
+fi
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Upgrade pip and install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Run migrations
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+
+# Start the server
+exec python manage.py runserver 0.0.0.0:8001
