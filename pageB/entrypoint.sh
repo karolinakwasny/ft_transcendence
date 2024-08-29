52e4c2979ebd250be99c326f3b10b4e15434facc
Niimphu <nimphu@outlook.com>
2024-08-29 12:29:53 +0200

    help

--- pageB/entrypoint.sh
+++ pageB/entrypoint.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
-
 # Check if virtual environment directory exists
 if [ ! -d "venv" ]; then
     # Create a virtual environment
     python3 -m venv venv
 fi
-
 # Activate the virtual environment
-source venv/bin/activate
-
+. venv/bin/activate
 # Upgrade pip and install dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
-
 # Run migrations
 python manage.py migrate
-
-# Start the server
+python manage.py collectstatic --noinput
+# Start the 
 exec python manage.py runserver 0.0.0.0:8001
