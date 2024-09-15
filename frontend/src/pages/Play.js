c41847d88e2a1cb3a86105a00c4876859d86056d
Niimphu <nimphu@outlook.com>
2024-09-15 17:21:06 +0200

    added individual pages

---
+++ frontend/src/pages/Play.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Play = () => {
+	return (
+		<div>
+			<h1>Play Page</h1>
+			<p>Ready to play the game? Click start!</p>
+		</div>
+	);
+};
+
+export default Play;
