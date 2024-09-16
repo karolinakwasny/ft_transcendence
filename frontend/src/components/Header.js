177fc1e5d04075f44d9ad476d5d537181a596b9b
Niimphu <nimphu@outlook.com>
2024-09-16 17:15:40 +0200

    clicking site name takes user back to home page

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -12,7 +12,9 @@ const Header = () => {
 				clip-rule="evenodd" class="icon" stroke="currentColor" >
 					<path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2z"/>
 				</svg>
-				<span>Pong</span>
+				<span className="siteName">
+					<Link to="/">Pong</Link>
+				</span>
 			</div>
 			<nav className="menu">
 				<Link to="/">Home</Link>
