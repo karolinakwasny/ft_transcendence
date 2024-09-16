1304f86a73f8bd76bbe0877dacbd23474e50d0c6
Niimphu <nimphu@outlook.com>
2024-09-16 16:01:18 +0200

    circle pong icon

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,7 +6,12 @@ const Header = () => {
 	return (
 		<header>
 			<div className="logo">
-				<img src="assets/gweeyam.png" alt="Pong Icon" className="icon" />
+				<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none"
+				stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+				viewBox= "0 0 24 24"
+				clip-rule="evenodd" class="icon" stroke="currentColor" >
+					<path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2z"/>
+				</svg>
 				<span>Pong</span>
 			</div>
 			<nav className="menu">
