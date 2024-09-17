a09b52fe0c60861e480fefcfc21b11c11cd59e27
Niimphu <nimphu@outlook.com>
2024-09-17 18:10:17 +0200

    home contents and styling

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Main from '../components/Main';
 import PlayButton from '../components/PlayButton';
+import './Home.css';
 
 const Home = () => {
 	return (
 		<div className="page-content">
 			<h1>HOME</h1>
-			<p>Welcome to the Pong Game!</p>
+			<div className="text-box left">
+				<p>The timeless classic, Pong, with a modern twist.</p>
+			</div>
+			<div className="text-box middle">
+				<p>Play locally with a friend, vs our AI, or against another player in head-to-head online multiplayer.</p>
+			</div>
+			<div className="text-box left">
+				<p>But not right now, website is still under construction :)</p>
+			</div>
 			<Main>
 				<PlayButton />
 			</Main>
