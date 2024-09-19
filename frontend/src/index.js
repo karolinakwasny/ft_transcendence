1de327e4a4e3f005008f86a1a51328602297f100
Niimphu <nimphu@outlook.com>
2024-09-19 19:34:39 +0200

    indentations

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-		<React.StrictMode>
+	<React.StrictMode>
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
