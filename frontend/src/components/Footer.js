46bf6e0dd5b75145cf457c2da22d0c18f5f05384
Niimphu <nimphu@outlook.com>
2024-09-20 15:37:56 +0200

    dark-mode toggle

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,9 +6,17 @@ const Footer = () => {
 		<footer className="footer">
 			<div className="container">
 				<div className="row">
-					<div className="col">
-						<a href="https://github.com/LukasKava/ft_transcendence" className="footer-link">Source code</a>
+					<div className="col d-flex justify-content-start">
+						<a href="https://github.com/LukasKava/ft_transcendence" className="footer-link">GitHub</a>
 						<a href="https://42wolfsburg.de/" className="footer-link">About 42</a>
+					</div>
+					<div className="col d-flex justify-content-end">
+						<input type="checkbox" className="checkbox" id="checkbox" />
+						<label for="checkbox" className="checkbox-label">
+							<i class="fas fa-sun" />
+							<i class="fas fa-moon" />
+							<span className="ball" />
+						</label>
 					</div>
 				</div>
 			</div>
