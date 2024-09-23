f18dc6e7ad47d67e53ff0b37584ba7a631067a32
Niimphu <nimphu@outlook.com>
2024-09-23 16:58:04 +0200

    fixed bug in hover and styles

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -9,8 +9,7 @@ const Header = () => {
 				<a className="navbar-brand logo" href="#">
 					<svg width="24" height="24	" xmlns="http://www.w3.org/2000/svg" fill="none"
 					strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-					viewBox= "0 0 24 24" clipRule="evenodd" className="icon" stroke="currentColor"
-					style={{color: "white"}} onMouseOver={{color: "white"}} >
+					viewBox= "0 0 24 24" clipRule="evenodd" className="icon" stroke="currentColor">
 						<path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2z"/>
 					</svg>
 					Pong
