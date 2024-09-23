cdb7241d66119a23da6d606cac32a38d411c86ed
Niimphu <nimphu@outlook.com>
2024-09-23 16:19:33 +0200

    header restyled with some BS

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,26 +4,26 @@ import './Header.css';
 
 const Header = () => {
 	return (
-		<header>
-			<div className="logo">
-				<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none"
-				strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-				viewBox= "0 0 24 24"
-				clipRule="evenodd" className="icon" stroke="currentColor" >
-					<path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2z"/>
-				</svg>
-				<span className="siteName">
-					<Link to="/">Pong</Link>
-				</span>
-			</div>
-			<nav className="menu">
-				<Link to="/">Home</Link>
-				<Link to="/play">Play</Link>
-				<Link to="/profile">Profile</Link>
-				<Link to="/about">About</Link>
+		<nav className="navbar navbar-expand-lg header">
+			<div className="container-fluid">
+				<a className="navbar-brand logo" href="#">
+					<svg width="24" height="24	" xmlns="http://www.w3.org/2000/svg" fill="none"
+					strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+					viewBox= "0 0 24 24" clipRule="evenodd" className="icon" stroke="currentColor"
+					style={{color: "white"}} onMouseOver={{color: "white"}} >
+						<path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2z"/>
+					</svg>
+					Pong
+				</a>
+				<nav className="ml-auto menu">
+					<Link to="/">Home</Link>
+					<Link to="/play">Play</Link>
+					<Link to="/profile">Profile</Link>
+					<Link to="/about">About</Link>
 				<a href="#" className="authenticate-button">Log in</a>
-			</nav>
-		</header>
+				</nav>
+			</div>
+		</nav>
 	);
 };
 
