ef0f201ae254e63c922bda90ac9816970f1105fb
LukasKava <luks.kav1231@gmail.com>
2024-10-22 04:56:25 +0300

    improvements over visuals

--- threejs_game/main.js
+++ threejs_game/main.js
@@ -7,6 +7,37 @@ import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
 import {OBJLoader} from 'three/addons/loaders/OBJLoader.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
+//Game start
+var	gameStart = false;
+
+//z direction x direction and speed
+var ballDirZ = 1, ballDirX = 1, ballSpeed = 0.0008;
+// paddle variables
+var paddleWidth, paddleHeight, paddleDepth, paddleQuality;
+var paddle1DirX = 0, paddle2DirX = 0, paddleSpeed = 3;
+var paddle1DirY = 0, paddle2DirY = 0;
+
+let player2Score = 0;
+let player1Score = 0;
+let canScorePlayer1 = true;
+let canScorePlayer2 = true;
+
+var WIDTH = 700,
+// HEIGHT = 500,
+// VIEW_ANGLE = 45,
+// ASPECT = WIDTH / HEIGHT,
+// NEAR = 0.1,
+// FAR = 10000,
+FIELD_WIDTH = 30,
+FIELD_LENGTH = 42,
+FIELD_HEIGHT = 10,
+BALL_RADIUS = 0.8,
+PLAYER_WIDTH = 4,
+PLAYER_HEIGHT = 1,
+OUTER_WALL_HEIGHT = 0.8,
+BALL_SPEED = 2,
+BALL_MOVE = true
+
 //TASKS
 //DISABLE 3rd dimension and 3rd dimension controls
 
@@ -22,16 +53,83 @@ const scene = new THREE.Scene();
 //I want the isometric view
 // Create an orthographic camera
 const aspect = window.innerWidth / window.innerHeight;
-const d = 20;
+// const d = 20;
 
-const camera = new THREE.OrthographicCamera(
-  -d * aspect,  // left
-  d * aspect,   // right
-  d,            // top
-  -d,           // bottom
-  1,            // near
-  1000          // far
-);
+// const camera = new THREE.OrthographicCamera(
+//   -d * aspect,  // left
+//   d * aspect,   // right
+//   d,            // top
+//   -d,           // bottom
+//   1,            // near
+//   1000          // far
+// );
+
+//Normal camera
+const camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
+camera.position.set(20, 20, 20);
+camera.lookAt(0, 1, 0); // Look at the center of the field
+
+//Adding directional lighting
+const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.5);
+
+
+
+directionalLight.shadow.mapSize.width = 512; // Size of the shadow map
+directionalLight.shadow.mapSize.height = 512;
+
+
+// Adjust shadow camera to control the width and height of the light's effect
+directionalLight.shadow.camera.left = -FIELD_WIDTH / 2; 
+directionalLight.shadow.camera.right = FIELD_WIDTH / 2; 
+directionalLight.shadow.camera.top = FIELD_LENGTH / 2; 
+directionalLight.shadow.camera.bottom = -FIELD_LENGTH / 2; 
+
+
+scene.add(directionalLight);
+directionalLight.position.set(0, 30, 0);
+directionalLight.castShadow = true;
+
+//directional light helper
+// const directionalLighthelper = new THREE.DirectionalLightHelper( directionalLight, 10 );
+// scene.add( directionalLighthelper );
+
+//Ambient light
+const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.2);
+scene.add(ambientLight);
+
+const spotLight = new THREE.SpotLight( 0xffffff, 800 );
+// spotLight.position.set( 0, 50, 0 );
+// spotLight.map = new THREE.TextureLoader().load( url );
+
+spotLight.castShadow = true;
+
+spotLight.shadow.mapSize.width = 2048;
+spotLight.shadow.mapSize.height = 2048;
+spotLight.shadow.camera.near = 10;
+spotLight.shadow.camera.far = 20;
+spotLight.shadow.camera.fov = 30;
+
+
+spotLight.position.set(0, 20, 0);  // Positioned above the field
+spotLight.target.position.set(0, 0, 0);  // Ensure the light targets the center of the field
+scene.add(spotLight.target);
+
+
+scene.add( spotLight );
+
+
+// //directional light helper
+
+// const spotLighthelper = new THREE.SpotLightHelper( spotLight, 10 );
+// scene.add( spotLighthelper );
+
+// Optional: Helper to visualize the shadow camera frustum
+// const shadowCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera);
+// scene.add(shadowCameraHelper);
+
+// const directionalCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+// scene.add(directionalCameraHelper);
+
 
 camera.position.set(20, 20, 20);
 
@@ -43,6 +141,9 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild(renderer.domElement);
+
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; // Optional: soft shadows
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -88,7 +189,7 @@ function createText(score, player) {
         });
 
         // Create a material for the text
-        const material = new THREE.MeshBasicMaterial({ color: 0x2E0E0E});
+        const material = new THREE.MeshStandardMaterial({ color: 0xfffff});
 
         // Create a mesh with the geometry and material
         currentTextMesh = new THREE.Mesh(tGeometry, material);
@@ -114,46 +215,50 @@ function updateScore(newScore, player) {
     }
 }
 
-//Game start
-var	gameStart = false;
-
-//z direction x direction and speed
-var ballDirZ = 1, ballDirX = 1, ballSpeed = 0.0008;
-// paddle variables
-var paddleWidth, paddleHeight, paddleDepth, paddleQuality;
-var paddle1DirX = 0, paddle2DirX = 0, paddleSpeed = 3;
-var paddle1DirY = 0, paddle2DirY = 0;
-
-let player2Score = 0;
-let player1Score = 0;
-let canScorePlayer1 = true;
-let canScorePlayer2 = true;
-
-var WIDTH = 700,
-// HEIGHT = 500,
-// VIEW_ANGLE = 45,
-// ASPECT = WIDTH / HEIGHT,
-// NEAR = 0.1,
-// FAR = 10000,
-FIELD_WIDTH = 30,
-FIELD_LENGTH = 42,
-FIELD_HEIGHT = 10,
-BALL_RADIUS = 0.8,
-PLAYER_WIDTH = 4,
-PLAYER_HEIGHT = 1,
-OUTER_WALL_HEIGHT = 0.8,
-BALL_SPEED = 2,
-BALL_MOVE = true
-
 
 //Player 1
 const	player1Geometry = new THREE.BoxGeometry(PLAYER_WIDTH, PLAYER_HEIGHT, 1);
-const	player1Material = new THREE.MeshBasicMaterial({color: 0xff0000});
+const	player1Material = new THREE.MeshStandardMaterial({color: 0xff0000});
 const	player1 = new THREE.Mesh(player1Geometry, player1Material);
+player1.castShadow = true;
 scene.add(player1);
 
 player1.position.z = FIELD_LENGTH/2 -1.5;
 player1.position.y = 1.6;
+
+//Player 1 sets
+
+const	player1Set1Geometry = new THREE.BoxGeometry(1, 1, 1);
+const	player1Set1Material = new THREE.MeshStandardMaterial({color: 0xff0000});
+const	player1Set1 = new THREE.Mesh(player1Set1Geometry, player1Set1Material);
+player1Set1.castShadow = true;
+scene.add(player1Set1);
+
+player1Set1.position.z = 2;
+player1Set1.position.y = 5;
+player1Set1.position.x = -FIELD_WIDTH / 2 -4;
+
+const	player1Set2Geometry = new THREE.BoxGeometry(1, 1, 1);
+const	player1Set2Material = new THREE.MeshStandardMaterial({color: 0xff0000});
+const	player1Set2 = new THREE.Mesh(player1Set2Geometry, player1Set2Material);
+player1Set2.castShadow = true;
+scene.add(player1Set2);
+
+player1Set2.position.z = 2;
+player1Set2.position.y = 7;
+player1Set2.position.x = -FIELD_WIDTH / 2 -4;
+
+
+const	player1Set3Geometry = new THREE.BoxGeometry(1, 1, 1);
+const	player1Set3Material = new THREE.MeshStandardMaterial({color: 0xff0000});
+const	player1Set3 = new THREE.Mesh(player1Set3Geometry, player1Set3Material);
+player1Set3.castShadow = true;
+scene.add(player1Set3);
+
+player1Set3.position.z = 2;
+player1Set3.position.y = 3;
+player1Set3.position.x = -FIELD_WIDTH / 2 -4;
+
 
 
 //Bounding Box Player1
@@ -167,12 +272,51 @@ console.log(player1BBox);
 
 //player2
 const player2Geometry = new THREE.BoxGeometry(PLAYER_WIDTH, PLAYER_HEIGHT, 1);
-const player2Material = new THREE.MeshBasicMaterial({color: 0xFFFFFF});
+const player2Material = new THREE.MeshStandardMaterial({color: 0xFFFFFF});
 const player2 = new THREE.Mesh(player2Geometry, player2Material);
+player2.castShadow = true;
 scene.add(player2);
 
 player2.position.z = -FIELD_LENGTH/2 + 1.5;
 player2.position.y = 1.6;
+
+//Player2 sets
+
+
+const	player2Set1Geometry = new THREE.BoxGeometry(1, 1, 1);
+const	player2Set1Material = new THREE.MeshStandardMaterial({color: 0xffffff});
+const	player2Set1 = new THREE.Mesh(player2Set1Geometry, player2Set1Material);
+player2Set1.castShadow = true;
+scene.add(player2Set1);
+
+player2Set1.position.z = -2;
+player2Set1.position.y = 5;
+player2Set1.position.x = -FIELD_WIDTH / 2 -4;
+
+const	player2Set2Geometry = new THREE.BoxGeometry(1, 1, 1);
+const	player2Set2Material = new THREE.MeshStandardMaterial({color: 0xffffff});
+const	player2Set2 = new THREE.Mesh(player2Set2Geometry, player2Set2Material);
+player1Set2.castShadow = true;
+scene.add(player2Set2);
+
+player2Set2.position.z = -2;
+player2Set2.position.y = 7;
+player2Set2.position.x = -FIELD_WIDTH / 2 -4;
+
+
+const	player2Set3Geometry = new THREE.BoxGeometry(1, 1, 1);
+const	player2Set3Material = new THREE.MeshStandardMaterial({color: 0xffffff});
+const	player2Set3 = new THREE.Mesh(player2Set3Geometry, player2Set3Material);
+player2Set3.castShadow = true;
+scene.add(player2Set3);
+
+player2Set3.position.z = -2;
+player2Set3.position.y = 3;
+player2Set3.position.x = -FIELD_WIDTH / 2 -4;
+
+
+
+
 
 
 //Bounding Box Player2
@@ -233,27 +377,30 @@ scene.add(line2);
 line2.position.z = -FIELD_LENGTH/2 + 2;
 line2.position.y = 0.1;
 
-//Cilinder 1 THE MIDDLE OF THE FIELD
+//Cilinder 2 THE MIDDLE OF THE FIELD
 const cylinder2Geometry = new THREE.CylinderGeometry( 4.5, 4.5, 0.7, 32 ); 
-const cylinder2Material = new THREE.MeshBasicMaterial( {color: 0x2C2F4B} ); 
+const cylinder2Material = new THREE.MeshStandardMaterial( {color: 0x2C2F4B} ); 
 const cylinder2 = new THREE.Mesh( cylinder2Geometry, cylinder2Material );
+cylinder2.receiveShadow = true;
 scene.add(cylinder2);
 
 //Cilinder 1 THE MIDDLE OF THE FIELD
 const cylinder1Geometry = new THREE.CylinderGeometry( 4.9, 4.9, 0.6, 32 ); 
 const cylinder1Material = new THREE.MeshBasicMaterial( {color: 0x000000} ); 
 const cylinder1 = new THREE.Mesh( cylinder1Geometry, cylinder1Material );
+cylinder1.receiveShadow = true;
 scene.add(cylinder1);
 
 //Inside of playing field
 const innerPLayingFieldGeometry = new THREE.BoxGeometry(FIELD_WIDTH - 1, 0.3, FIELD_LENGTH - 1);
-const innerPLayingFieldMaterial = new THREE.MeshBasicMaterial({color: 0x2C2F4B});
+const innerPLayingFieldMaterial = new THREE.MeshStandardMaterial({color: 0x2C2F4B});
 const innerPLayingField = new THREE.Mesh(innerPLayingFieldGeometry, innerPLayingFieldMaterial);
+innerPLayingField.receiveShadow = true;
 scene.add(innerPLayingField);
 
 //Outer playing field wall z1
 let	outerWallZ1Geometry = new THREE.BoxGeometry(FIELD_WIDTH, OUTER_WALL_HEIGHT, 0.5);
-let	outerWallZ1Material = new THREE.MeshBasicMaterial({color: 0x90384A});
+let	outerWallZ1Material = new THREE.MeshStandardMaterial({color: 0x90384A});
 let	outerWallZ1 = new THREE.Mesh(outerWallZ1Geometry, outerWallZ1Material);
 scene.add(outerWallZ1);
 
@@ -261,7 +408,7 @@ outerWallZ1.position.z = (-FIELD_LENGTH/2) + (0.5/2);
 
 //Outer playing field wall z2
 let	outerWallZ2Geometry = new THREE.BoxGeometry(FIELD_WIDTH, OUTER_WALL_HEIGHT, 0.5);
-let	outerWallZ2Material = new THREE.MeshBasicMaterial({color: 0x90384A});
+let	outerWallZ2Material = new THREE.MeshStandardMaterial({color: 0x90384A});
 let	outerWallZ2 = new THREE.Mesh(outerWallZ2Geometry, outerWallZ2Material);
 scene.add(outerWallZ2);
 
@@ -269,7 +416,7 @@ outerWallZ2.position.z = (FIELD_LENGTH/2) - (0.5/2);
 
 //Outer playing field wall x1
 let	outerWallX1Geometry = new THREE.BoxGeometry(0.5, OUTER_WALL_HEIGHT, FIELD_LENGTH-1);
-let	outerWallX1Material = new THREE.MeshBasicMaterial({color: 0x1F2135});
+let	outerWallX1Material = new THREE.MeshStandardMaterial({color: 0x1F2135});
 let	outerWallX1 = new THREE.Mesh(outerWallX1Geometry, outerWallX1Material);
 scene.add(outerWallX1);
 
@@ -277,7 +424,7 @@ outerWallX1.position.x = (-FIELD_WIDTH/2) + (0.5/2);
 
 //Outer playing field wall x2
 let	outerWallX2Geometry = new THREE.BoxGeometry(0.5, OUTER_WALL_HEIGHT, FIELD_LENGTH-1);
-let	outerWallX2Material = new THREE.MeshBasicMaterial({color: 0x1F2135});
+let	outerWallX2Material = new THREE.MeshStandardMaterial({color: 0x1F2135});
 let	outerWallX2 = new THREE.Mesh(outerWallX2Geometry, outerWallX2Material);
 scene.add(outerWallX2);
 
@@ -289,15 +436,16 @@ const cube1Material = new THREE.MeshBasicMaterial({color: 0xFFFFF});
 const cube1 = new THREE.Mesh(cube1Geometry, cube1Material);
 scene.add(cube1);
 
-const cube2Geometry = new THREE.BoxGeometry(FIELD_WIDTH, 10, FIELD_LENGTH);
-const cube2Material = new THREE.MeshBasicMaterial({color: 0x00ff0, wireframe: true});
-const cube2 = new THREE.Mesh(cube2Geometry, cube2Material);
-scene.add(cube2);
-cube2.position.y +=  5;
+// const cube2Geometry = new THREE.BoxGeometry(FIELD_WIDTH, 10, FIELD_LENGTH);
+// const cube2Material = new THREE.MeshBasicMaterial({color: 0x00ff0}); //, wireframe: true
+// const cube2 = new THREE.Mesh(cube2Geometry, cube2Material);
+// scene.add(cube2);
+// cube2.position.y +=  5;
 
 const ballGeometry = new THREE.SphereGeometry(0.8, 20, 20);
-const ballMaterial = new THREE.MeshBasicMaterial({ color: 0xCC0000 });
+const ballMaterial = new THREE.MeshStandardMaterial({ color: 0xCC0000 });
 const ball = new THREE.Mesh(ballGeometry, ballMaterial);
+ball.castShadow = true;
 scene.add(ball);
 
 const ballBoundingSphere = new THREE.Sphere(ball.position, BALL_RADIUS);
@@ -353,7 +501,7 @@ function	annouceWinner(player) {
 function	ballMovement() {
 	//Player 2 scores
 	if (ball.position.z >= FIELD_LENGTH / 2) {
-		outerWallZ2.material.color.set(0xE84646);
+		outerWallZ2.material.color.set(0xFFFFFF);
 		player2Score++;
 		updateScore(player2Score, 2);
 		if (player2Score == 10) {
@@ -368,7 +516,7 @@ function	ballMovement() {
 
 	//Player 1 scores
 	if (ball.position.z <= -FIELD_LENGTH / 2) {
-		outerWallZ1.material.color.set(0xE84646);
+		outerWallZ1.material.color.set(0xFF0000);
 		player1Score++;
 		updateScore(player1Score, 1);
 		if (player1Score == 10) {
@@ -413,7 +561,7 @@ const keysPressed = {};
 
 document.body.addEventListener("keydown", (e) => {
   keysPressed[e.key] = true;
-  console.log("KeyPressed: ", keysPressed);
+  //console.log("KeyPressed: ", keysPressed);
   movePlayer();
 });
 
@@ -514,8 +662,31 @@ function ballMove() {
 	}
 }
 
+function rotateSets() {
+	player1Set1.rotation.x -= 0.01;
+	player1Set2.rotation.x += 0.01;	
+	player1Set3.rotation.x += 0.01;
+
+	player1Set1.rotation.y += 0.01;
+	player1Set2.rotation.y -= 0.01;	
+	player1Set3.rotation.y += 0.01;
+
+
+	player2Set1.rotation.x += 0.01;
+	player2Set2.rotation.x -= 0.01;	
+	player2Set3.rotation.x += 0.01;
+
+	player2Set1.rotation.y += 0.01;
+	player2Set2.rotation.y += 0.01;	
+	player2Set3.rotation.y -= 0.01;
+
+
+}
+
 function animate() {
 	renderer.render(scene, camera);
+
+	rotateSets();
 	renderer.setAnimationLoop(animate);
 	if (gameStart == false) {
 		startBallMovement();
