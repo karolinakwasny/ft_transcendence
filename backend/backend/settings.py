1c750a928e88dbc37b5c8668d2367d154fb1f42f
ekuchel <ekuchel@student.42wolfburg.de>
2024-11-08 14:50:51 +0100

    env update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -32,9 +32,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-x1a#yw-&_gh&jvp06gn)m2x-d@_z06ghuygo$^!f5s8g+)_mql'
+# SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
