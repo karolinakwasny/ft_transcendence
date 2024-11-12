8c4b371474a19c9dba6db02fbfd446258ff5ebbf
Niimphu <nimphu@outlook.com>
2024-11-12 14:05:24 +0100

    notif menu looking good

--- frontend/src/components/NotifMenu.js
+++ frontend/src/components/NotifMenu.js
@@ -7,10 +7,10 @@ const NotifMenu = () => {
 			<button onClick={openDropdown} className="btn btn-secondary dropbtn">
 				<i className="fas fa-bell bell-icon"></i>
 			</button>
-			<div id="drop" className="dropdown-content">
-				<a className="dropdown-item" href="#">A</a>
-				<a className="dropdown-item" href="#">B</a>
-				<a className="dropdown-item" href="#">C</a>
+			<div id="drop" className="dropdown-content mt-1">
+				<a className="dropdown-item" href="#">Notification 1</a>
+				<a className="dropdown-item" href="#">Notification 2</a>
+				<a className="dropdown-item" href="#">Notification 3</a>
 			</div>
 		</div>
 	);
