e01b5c85709cc7e37542cba3205cf3a7d9d88bfe
Niimphu <nimphu@outlook.com>
2024-11-25 18:37:37 +0100

    started changing layout

--- frontend/src/pages/LogIn.js
+++ frontend/src/pages/LogIn.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import './Login.css';
 import LogInButton from '../components/LogInButton';
 import { useTranslation } from "react-i18next";
 
 const LogIn = () => {
 	const {t} = useTranslation();
 	return (
-		<div className="page-content">
+		<div className="page-content container login">
 			<h1>{t("LogInTitle")}</h1>
-			<p>
-				{t("LogInText")}
-			</p>
-			<div className="col d-flex justify-content-center align-items-center">
-				<LogInButton />
+			<div className="login-box">
+				<p>
+					{t("LogInText")}
+				</p>
+				<div className="col d-flex justify-content-center align-items-center">
+					<LogInButton />
+				</div>
+
 			</div>
 		</div>
 	);
