ad27a74b2648b584e82ee078b6806c48d54441b5
Niimphu <nimphu@outlook.com>
2024-11-25 17:55:16 +0100

    removed old functionality, added comment

--- frontend/src/components/LogInButton.js
+++ frontend/src/components/LogInButton.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
 import './LogInButton.css';
 import { useTranslation } from "react-i18next";
 
 const LogInButton = () => {
-	const history = useHistory();
 	const {t} = useTranslation();
 
-	const handleClick = () => {
-		history.push('/login');
-	}
-
 	return (
+		// target blank opens link in new tab
 		<a href="https://www.google.co.uk" target="_blank">
 			<button className="btn button login-button py-2 px-5 mt-5">
 				{t("LogInButton")}
