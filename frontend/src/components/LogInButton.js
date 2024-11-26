6081500c52cf74d6a769ade70fc56c742ec7dd21
Niimphu <nimphu@outlook.com>
2024-11-26 19:28:56 +0100

    lovely new login page

--- frontend/src/components/LogInButton.js
+++ frontend/src/components/LogInButton.js
@@ -11,7 +11,7 @@ const LogInButton = () => {
 	}
 
 	return (
-		<button className="btn button login-button py-2 px-5 mt-5" onClick={handleClick}>
+		<button className="btn button login-button42 py-2 px-5" onClick={handleClick}>
 			{t("LogInButton")}
 		</button>
 	);
--- frontend/src/pages/LogIn.js
+++ frontend/src/pages/LogIn.js
@@ -1,21 +1,74 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Login.css';
+import '../components/Button.css'
 import LogInButton from '../components/LogInButton';
 import { useTranslation } from "react-i18next";
 
 const LogIn = () => {
 	const {t} = useTranslation();
-	return (
-		<div className="page-content container login">
-			<h1>{t("LogInTitle")}</h1>
-			<div className="login-box">
-				<p>
-					{t("LogInText")}
-				</p>
-				<div className="col d-flex justify-content-center align-items-center">
-					<LogInButton />
-				</div>
 
+	const [isSignUp, setIsSignUp] = useState(false);
+
+	const handleCheckboxChange =() => {
+		setIsSignUp(prevState => !prevState);
+	}
+
+	return (
+		<div className="page-content container login mt-5">
+			<h1 className="login-title">
+				{t("LogInTitle")}
+			</h1>
+			<div className="login-box container p-4">
+				<div className="login-toggle">
+					<input
+						id="status"
+						type="checkbox"
+						name="status"
+						checked={isSignUp}
+						onChange={handleCheckboxChange}
+					/>
+					<label htmlFor="status" className="status-label m-0">
+						<div className="status-switch m-0 p-0">
+							<div className="highlight"></div>
+							<span className="text unchecked">{t("LogInText")}</span>
+							<span className="text checked">{t("LogInText2")}</span>
+						</div>
+					</label>
+				</div>
+				<div className="login-field">
+					<p className="field-name">
+						{t("LogInText3")}
+						<input className="text-field form-control"/>
+					</p>
+					{isSignUp && (
+						<p className="field-name">
+							{t("LogInText6")}
+							<input className="text-field form-control" type="email"/>
+						</p>
+					)}
+					<p className="field-name">
+						{t("LogInText4")}
+						<input className="text-field form-control" type="password"/>
+					</p>
+					{isSignUp && (
+						<p className="field-name">
+							{t("LogInText5")}
+							<input className="text-field form-control" type="password"/>
+						</p>
+					)}
+					<div className="login-buttons">
+						<div className="button login-button m-3 px-3 py-1">
+							{
+								isSignUp ? (
+									<div>{t("LogInText2")}</div>
+								) : (
+									<div>{t("LogInText")}</div>
+								)
+							}
+						</div>
+						<LogInButton/>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
