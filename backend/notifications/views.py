df23de4c16168afe0ddf129f7307c9c96c31f36d
ekuchel <ekuchel@student.42wolfburg.de>
2024-12-05 12:43:36 +0100

    update

--- backend/notifications/views.py
+++ backend/notifications/views.py
@@ -3,8 +3,19 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Notification
 from .serializers import NotificationSerializer
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 # Create your views here.
 
+def send_friend_request_notification(user, message):
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(
+        f'user_{user.id}',  # Group name
+        {
+            'type': 'friend_request',
+            'message': message
+        }
+    )
 
 class NotificationViewSet(viewsets.ModelViewSet):
     queryset = Notification.objects.all()
