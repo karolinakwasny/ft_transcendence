ef37ca42104f019f54ca83e16f0c97b4051fb269
Erwin Küchel <e.kuechel>
2025-02-19 17:41:31 +0100

    corrected model for tournament property

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -407,6 +407,8 @@ class TournamentSerializer(serializers.Serializer):
             description=validated_data.get('description', ''),
             host=host
         )
+        player_profile.tournament = tournament
+        player_profile.save()
     
         player_profiles = PlayerProfile.objects.filter(user_id__in=player_ids)
     
@@ -488,6 +490,7 @@ class ExitTournamentSerializer(serializers.Serializer):
 
         return attrs
 
+    @staticmethod
     def delete_tournament_and_update_players(tournament_id):
         try:
             tournament = Tournament.objects.get(id=tournament_id)
@@ -517,7 +520,7 @@ class ExitTournamentSerializer(serializers.Serializer):
         player.save()
 
         if player.is_host:
-            delete_tournament_and_update_players(tournament.id) 
+            ExitTournamentSerializer.delete_tournament_and_update_players(tournament.id)
             return {"message": "Tournament has been destroyed due to the host leaving"}
 
         if match:
@@ -639,6 +642,7 @@ class ScoreRetrieveSerializer(serializers.Serializer):
             player_winner.save()
             player_host = PlayerProfile.objects.get(user_id=curr_tournament.host)
             player_host.is_host = False
+            player_host.tournament = None
             player_host.save()
             return {"message": f"{curr_tournament.champion} is this tournament's champion"}
 
