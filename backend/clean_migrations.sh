8e15d4b427cf8c2f1ed2d2a42e36f2c573d7ccf8
Erwin Küchel <e.kuechel>
2025-02-27 17:47:47 +0100

    media redir nginx / makefile correction

--- backend/clean_migrations.sh
+++ backend/clean_migrations.sh
@@ -4,9 +4,4 @@ find backend/friends/migrations -type f -name "*.py" ! -name "__init__.py" -dele
 
 echo "Migration files removed."
 
-find backend/media/avatars/ -type f -name "*" ! -name "avatar.png" -delete
-echo "Profile pictures removed."
-
-rm -rf backend/media/qrcode/*
-echo "QR code images removed."
 
