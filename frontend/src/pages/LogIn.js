2ece2c7c16bdc1a45f2c26a845188fdc36f76cf6
Erwin Küchel <e.kuechel>
2025-02-27 20:54:07 +0100

    update username already exists

--- frontend/src/pages/LogIn.js
+++ frontend/src/pages/LogIn.js
@@ -100,7 +100,7 @@ const LogIn = () => {
 					if (error.response.data.username) {
 						if (error.response.data.username[0] === 'Username must be 3-30 characters long and can only contain letters, numbers, underscores, hyphens, and periods.')
 							alert(t('Username must be 3-30 characters long and can only contain letters, numbers, underscores, hyphens, and periods.'));
-						else if (error.response.data.username[0] === 'A user with that username already exists.')
+						else if (error.response.data.username[0] === 'Username already exists!')
 							alert(t('Username already exists. Please choose a different one.'));
 					}else if(error.response.data.email){
 						alert(t('Email was already used. Please choose a different one.'));
