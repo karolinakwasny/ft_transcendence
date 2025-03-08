f219553353fd97c049c634bff9fcfef40a98e3fa
karolinakwasny <kwasnykarolina98@gmail.com>
2025-03-08 01:02:30 +0100

    log in / sighn up changed, added an error for confirm password

--- frontend/src/pages/LogIn.js
+++ frontend/src/pages/LogIn.js
@@ -28,6 +28,7 @@ const LogIn = () => {
 	const [password, setPassword] = useState('');
 	const [email, setEmail] = useState('');
 	const [errorOTP, setErrorOTP] = useState('');
+	const [errorPassword, setErrorPassword] = useState('');
 	const [firstName, setFirstName] = useState('');
 	const [lastName, setLastName] = useState('');
 	const [confirmPassword, setConfirmPassword] = useState('');
@@ -71,6 +72,7 @@ const LogIn = () => {
   	};
 
 	const handleSubmit = async (event) => {
+		setErrorPassword('');
 		event.preventDefault();
 
 		const signup_url = baseUrl + '/auth/users/' //api for new user registration
@@ -89,7 +91,11 @@ const LogIn = () => {
 		//debugging purposes end
 
 		if (isSignUp) {
-			
+			if (password !== confirmPassword) {
+				setErrorPassword(t('Password and confirm password do not match.'));
+				return;
+			}
+
 			try {
 				const response = await axiosInstance.post(signup_url, {
 					username,
@@ -104,26 +110,26 @@ const LogIn = () => {
 					
 					if (error.response.data.username) {
 						if (error.response.data.username[0] === 'Username must be 3-30 characters long and can only contain letters, numbers, underscores, hyphens, and periods.')
-							alert(t('Username must be 3-30 characters long and can only contain letters, numbers, underscores, hyphens, and periods.'));
+							setErrorPassword(t('Username must be 3-30 characters long and can only contain letters, numbers, underscores, hyphens, and periods.'));
 						else if (error.response.data.username[0] === 'Username already exists!')
-							alert(t('Username already exists. Please choose a different one.'));
+							setErrorPassword(t('Username already exists. Please choose a different one.'));
 					}else if(error.response.data.email){
 						if (error.response.data.email[0] === 'Email already exists!')
-							alert(t('Email was already used. Please choose a different one.'));
+							setErrorPassword(t('Email was already used. Please choose a different one.'));
 						else
-							alert(t('Invalid format of the email.'))
+							setErrorPassword(t('Invalid format of the email.'))
 					}else if (error.response.data.password){
 						if (error.response.data.password[0] === 'This password is too short. It must contain at least 8 characters.')
-							alert(t('This password is too short. It must contain at least 8 characters.'));
+							setErrorPassword(t('This password is too short. It must contain at least 8 characters.'));
 						else if (error.response.data.password[0] === 'This password is too common.')
-							alert(t('This password is too common.'));
+							setErrorPassword(t('This password is too common.'));
 						else if (error.response.data.password[0] === 'This password is entirely numeric.')
-							alert(t('This password is entirely numeric.'));
+							setErrorPassword(t('This password is entirely numeric.'));
 						else 
-							alert(t('Email must be in the format: example@domain.com'));
+							setErrorPassword(t('Email must be in the format: example@domain.com'));
 					
 					} else {
-						alert(t('Sign up failed. Please try again.'));
+						setErrorPassword(t('Sign up failed. Please try again.'));
 					}
 				}
 			}
@@ -149,7 +155,7 @@ const LogIn = () => {
 					setShowOTPModal(true);
 					} else {
 					console.error('Error logging in:', error);
-					alert(t("Login failed. Please check your credentials."));
+					setErrorPassword(t("Login failed. Please check your credentials."));
 					}
 				}
 			}	
@@ -161,22 +167,25 @@ const LogIn = () => {
 				<h1 className="login-title pageHeadingH1Style1">
 					{t("LogInTitle")}
 				</h1>
-				<div className="loginCardHolderStyle"> {/*container login-box p-4 */}
-					<div className="login-toggle">
-						<input
-							id="status"
-							type="checkbox"
-							name="status"
-							checked={isSignUp}
-							onChange={handleCheckboxChange}
-						/>
-						<label htmlFor="status" className="status-label m-0">
-							<div className="status-switch m-0 p-0">
-								<div className="highlight"></div>
-								<span className="text unchecked">{t("LogInText")}</span>
-								<span className="text checked">{t("LogInText2")}</span>
-							</div>
-						</label>
+				<div className="loginCardHolderStyle"> 
+					<div className='welcome-text' >{t("Sign in to your account or create a new one")}</div>
+					<div>
+						<div className="login-toggle">
+							<input
+								id="status"
+								type="checkbox"
+								name="status"
+								checked={isSignUp}
+								onChange={handleCheckboxChange}
+							/>
+							<label htmlFor="status" className="status-label m-0">
+								<div className="status-switch m-0 p-0">
+									<div className="highlight"></div>
+									<span className="text unchecked">{t("LogInText")}</span>
+									<span className="text checked">{t("LogInText2")}</span>
+								</div>
+							</label>
+						</div>
 					</div>
 					<form onSubmit={handleSubmit} className="w-100">
 						<div className="login-field">
@@ -228,9 +237,11 @@ const LogIn = () => {
 									)
 							}
 							<div>
+								{errorPassword && <p className="tfa-message">{errorPassword}</p>}
 								<button type="submit" className="buttonStyle1" aria-label={isSignUp ? t("SignUpButton") : t("LogInButton")} > {/*btn button login-button py-2 px-5*/}
 									{isSignUp ? t("LogInText2") : t("LogInText")}
 								</button>
+								
 								<LogInButton/>
 							</div>
 						</div>
