b9760b04241e17db0ef593100f3232c38f4688c3
karolinakwasny <kwasnykarolina98@gmail.com>
2025-03-21 23:10:47 +0100

    this should work

--- backend/script.sh
+++ backend/script.sh
@@ -9,17 +9,17 @@ if [ -f .env ]; then
     export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
 fi
 
-python3 manage.py makemigrations users
-python3 manage.py makemigrations
-python3 manage.py migrate --noinput --run-syncdb
+python3 backend/manage.py makemigrations users
+python3 backend/manage.py makemigrations
+python3 backend/manage.py migrate --noinput --run-syncdb
 
-python3 manage.py create_users
+python3 backend/manage.py create_users
 
-python3 manage.py collectstatic --noinput
+python3 backend/manage.py collectstatic --noinput
 
 
 #gunicorn backend.wsgi:application --bind 0.0.0.0:8000 --workers 3
-python3 manage.py runserver 0.0.0.0:8000
+python3 backend/manage.py runserver 0.0.0.0:8000
 #python manage.py runsslserver --use_ssl
 #python3 manage.py runserver_plus --cert-file /certs/fullchain.crt --key-file /certs/privkey.key 8000
 
