63382880ec825063b13243517ebecec30311d74f
karolinakwasny <kwasnykarolina98@gmail.com>
2025-03-22 02:10:43 +0100

    trying out nginx

--- nginx/script.sh
+++ nginx/script.sh
@@ -61,9 +61,20 @@ create_env() {
     echo "✅ Production .env file created."
 }
 
+configure_nginx() {
+    echo "🔄 Replacing environment variables in NGINX configuration..."
+    envsubst '$NGINX_SERVER_NAME' < /etc/nginx/nginx.template > /tmp/nginx.conf
+    echo "✅ NGINX configuration updated."
+
+    # Start NGINX with the new configuration
+    echo "🚀 Starting NGINX..."
+    nginx -c /tmp/nginx.conf -g "daemon off;"
+}
+
 # Run all functions
 generate_certificates
 create_env
 copy_env
+configure_nginx
 
 echo "✅ Render process completed successfully!"
