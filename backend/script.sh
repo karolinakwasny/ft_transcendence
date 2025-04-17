a7e03c85f2b8dc4f61a1806c71ee6055a7b25d3c
karolinakwasny <kwasnykarolina98@gmail.com>
2025-04-17 15:11:46 +0200

    no old url

--- backend/script.sh
+++ backend/script.sh
@@ -4,12 +4,11 @@ echo "
     Script for backend
 "
 
-
 python3 backend/manage.py makemigrations users
 python3 backend/manage.py makemigrations
 python3 backend/manage.py migrate --noinput --run-syncdb
 
-python3 backend/manage.py create_users
+# python3 backend/manage.py create_users
 
 python3 backend/manage.py collectstatic --noinput
 python3 backend/manage.py delete_expired_qrcodes
