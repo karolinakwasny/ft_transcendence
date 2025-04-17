e98dd1f34ca6d643755800049bc1703209132f73
karolinakwasny <kwasnykarolina98@gmail.com>
2025-04-17 13:46:50 +0200

    that was wrong

--- backend/script.sh
+++ backend/script.sh
@@ -9,18 +9,6 @@ if [ -f .env ]; then
     export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
 fi
 
-# Add this to your Render deploy script
-python3 backend/manage.py makemigrations users --empty --name add_qr_code_generated_at
-
-# Then inside the generated migration file (e.g. `000X_add_qr_code_generated_at.py`) in users/migrations/
-# manually add this:
-migrations.AddField(
-    model_name='user',
-    name='qr_code_generated_at',
-    field=models.DateTimeField(blank=True, null=True),
-)
-
-
 python3 backend/manage.py makemigrations users
 python3 backend/manage.py makemigrations
 python3 backend/manage.py migrate --noinput --run-syncdb
