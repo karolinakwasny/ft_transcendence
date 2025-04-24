2ab3c0bdf5e098778d4161c8eef9b66bfaf64f5d
karolinakwasny <kwasnykarolina98@gmail.com>
2025-04-24 12:52:06 +0200

    yes

--- frontend/src/components/ProfileComponents/ListFriends.js
+++ frontend/src/components/ProfileComponents/ListFriends.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { getAllPlayers } from '../../services/getAllUsers';
 import { useTranslation } from 'react-i18next';
 
